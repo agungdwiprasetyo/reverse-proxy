630865fe9f1990a282d1e4b4c344596d53785a79
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2019-03-18 11:03:26 +0700

    add unit test and integrate travis ci & codecov

--- Makefile
+++ Makefile
@@ -1,7 +1,17 @@
-.PHONY : build test
+.PHONY : build test cover
+
+TEST_PACKAGES = ./helper
 
 build:
 	go build -o bin
 
 test:
-	go test -v
+	$(foreach pkg, $(TEST_PACKAGES),\
+	go test $(pkg);)
+
+cover:
+	if [ -f coverage.txt ]; then rm coverage.txt; fi;
+	$(foreach pkg, $(TEST_PACKAGES), \
+	go test -coverprofile=coverage.out -covermode=atomic $(pkg); \
+	tail -n +2 coverage.out >> coverage.txt;)
+	rm coverage.out
---
+++ helper/color_test.go
@@ -0,0 +1,97 @@
+package helper
+
+import (
+	"testing"
+)
+
+func TestColorForStatus(t *testing.T) {
+	tests := []struct {
+		name string
+		args int
+		want string
+	}{
+		{
+			name: "Testcase #1: Return Green",
+			args: 200,
+			want: Green,
+		},
+		{
+			name: "Testcase #2: Return White",
+			args: 300,
+			want: White,
+		},
+		{
+			name: "Testcase #3: Return Yellow",
+			args: 400,
+			want: Yellow,
+		},
+		{
+			name: "Testcase #4: Return Red",
+			args: 500,
+			want: Red,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ColorForStatus(tt.args); got != tt.want {
+				t.Errorf("ColorForStatus() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestColorForMethod(t *testing.T) {
+	tests := []struct {
+		name string
+		args string
+		want string
+	}{
+		{
+			name: "Testcase #1: Return Blue",
+			args: "GET",
+			want: Blue,
+		},
+		{
+			name: "Testcase #2: Return Cyan",
+			args: "POST",
+			want: Cyan,
+		},
+		{
+			name: "Testcase #3: Return Yellow",
+			args: "PUT",
+			want: Yellow,
+		},
+		{
+			name: "Testcase #4: Return Red",
+			args: "DELETE",
+			want: Red,
+		},
+		{
+			name: "Testcase #5: Return Green",
+			args: "PATCH",
+			want: Green,
+		},
+		{
+			name: "Testcase #6: Return Magenta",
+			args: "HEAD",
+			want: Magenta,
+		},
+		{
+			name: "Testcase #7: Return White",
+			args: "OPTIONS",
+			want: White,
+		},
+		{
+			name: "Testcase #8: Return RESET",
+			args: "RESET",
+			want: Reset,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ColorForMethod(tt.args); got != tt.want {
+				t.Errorf("ColorForMethod() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
